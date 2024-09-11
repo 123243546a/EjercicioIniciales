7fb65985b35885ca1a655dcb83b844cc02e970c4
123243546a <adrianrodbar@gmail.com>
2024-09-11 20:32:27 +0200

    ej1 y 2

---
+++ Assets/Scripts/Ejercicio2_1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    int edad = 19;
+    float altura =1.82f;
+    string nombre = "Adrian";
+    float iva = 18.93f;
+    float peso = 62;
+    bool alumnoRepetidor = true;
+    string letra = "a";
+    int minutos = 12;
+    int matriculaCoche = 928472838;
+    int codigoPostal = 28053;
+    bool genero = true;
+    int numeroDeHijos = 2;
+    int tallaCamisa = 50;
+    float precio = 29.50f;
+    string mensaje = "Nose que decir";
+    bool mayorDeEdad = false;
+    int dias = 30;
+    float contador = 63.29f;
+    char tallaCamiseta = 'L';
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Ejercicio_1.cs
+++ Assets/Scripts/Ejercicio_1.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Ejercicio_1 : MonoBehaviour
 {
+    //Declarar la variable(nombre de la caja), asignación de la variable (el número de cosas que metes en la caja)
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("¡Hola mundo!");
+        Debug.Log("Este es el primer videojuego de Adrián");
+        Debug.Log("Estoy aprendiendo C#");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame");
+        //Porque lo hemos puesto en Update y eso significa que se actualiza cada frame, es decir aparece X veces por segundo dependiendo de los fps.
     }
 }
